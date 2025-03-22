dfc901d55bd0c3e4055b526fa8206608f41ed79e
Dearie <eburujacintha@gmail.com>
2025-03-22 11:52:43 +0400

    added global styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
       "./src/**/*.{js,jsx,ts,tsx,mdx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'dark-gray': '#404040',
+            'charcoal': '#1e1e1e',
+            'accent-red': '#f10000',
+            'accent-red-dark': '#d90000',
+            'light-gray': '#d9d9d9',
+            'near-black': '#0d0d0d',
+            'primary-blue': '#2a90ef',
+            'primary-blue-dark': '#1a7cd9',
+            'dark': '#212121',
+          },
+          fontFamily: {
+            'montserrat': ['Montserrat', 'sans-serif'],
+            'roboto': ['Roboto', 'sans-serif'],
+            'poppins': ['Poppins', 'sans-serif'],
+          },
+      },
     },
     plugins: [],
   };
