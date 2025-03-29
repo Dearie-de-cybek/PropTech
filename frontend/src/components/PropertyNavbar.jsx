8af87b94cf0d4808c27957e7367d7f4d8b347224
Dearie <eburujacintha@gmail.com>
2025-03-29 11:54:25 +0400

    corrected icons

--- frontend/src/components/PropertyNavbar.jsx
+++ frontend/src/components/PropertyNavbar.jsx
@@ -21,9 +21,7 @@ const PropertyNavbar = () => {
         <div className="relative flex-1 mx-8">
           <div className="bg-[#1E1E1E] rounded-[20px] flex items-center px-4 py-2">
             {/* Home Icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-            </svg>
+            <img src="../public/icons/home.svg" alt="" className="h-5 w-5 text-white mr-2"/>
             
             {/* Search Input */}
             <input
@@ -33,9 +31,7 @@ const PropertyNavbar = () => {
             />
             
             {/* Search Icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
+            <img src="../public/icons/search.svg" alt="" className="h-5 w-5  ml-2"/>
           </div>
         </div>
         
