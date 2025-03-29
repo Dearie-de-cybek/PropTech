ef526ca8782d9614f54e3acde6a6d6d168dc36ff
Dearie <eburujacintha@gmail.com>
2025-03-29 23:31:01 +0400

    would review

--- frontend/src/components/PropertyNavbar.jsx
+++ frontend/src/components/PropertyNavbar.jsx
@@ -9,7 +9,7 @@ const PropertyNavbar = () => {
         <div className="flex items-center space-x-4">
           {/* Hamburger Menu */}
           <button className="text-white focus:outline-none">
-            <img src="../public/icons/hambug.svg" alt="" />
+            <img src="../public/icons/hambug.svg" alt="" className="h-6 w-6"/>
           </button>
           
           {/* Logo */}
@@ -18,15 +18,15 @@ const PropertyNavbar = () => {
         </div>
         
         {/* Middle - Search Bar */}
-        <div className="relative flex-1 mx-8">
-          <div className="bg-[#1E1E1E] rounded-[20px] flex items-center px-4 py-2">
+        <div className="relative w-[600px] mx-8  rounded-[20px]">
+          <div className="bg-[#1E1E1E] flex items-center px-4 py-2">
             {/* Home Icon */}
             <img src="../public/icons/home.svg" alt="" className="h-5 w-5 text-white mr-2"/>
             
             {/* Search Input */}
             <input
               type="text"
-              placeholder="Search properties..."
+              placeholder="Enter Address, neighbourhood, city or postal code"
               className="bg-transparent w-full text-white font-roboto focus:outline-none"
             />
             
@@ -37,11 +37,11 @@ const PropertyNavbar = () => {
         
         {/* Right - Register and Login */}
         <div className="flex space-x-6">
-          <Link to="/register" className="text-white font-roboto uppercase">
+          <Link to="/register" className="text-blue-500 font-semibold font-montserrat">
             REGISTER
           </Link>
-          <Link to="/login" className="text-white font-roboto uppercase">
-            LOGIN
+          <Link to="/login" className="text-white font-semibold font-montserrat">
+            LOG IN
           </Link>
         </div>
       </div>
